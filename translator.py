6f3f2e1b004e6057e307fc76cd4206487b370f30
Mikołaj Haczyk <61386483+glo51@users.noreply.github.com>
2020-12-28 14:05:04 +0100

    Add files via upload

--- translator.py
+++ translator.py
@@ -1,31 +1,34 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
-import re
-import sys
-
+from re import compile
+from sys import argv
 
 languages = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese',
              'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish']
 
 
-def manual():
+def console():
     print("Hello, you're welcome to the translator. Translator supports:")
     for i, language in enumerate(languages, 1):
         print(f'{i}. {language}')
 
-    source_language = int(input('Type the number of your language: '))
-    if source_language not in range(1, 14):
-        print('Wrong number!')
-        return 0
-    source_language = languages[source_language-1]
+    while True:
+        source_language = int(input('Type the number of your source language: '))
+        if source_language not in range(1, 14):
+            print('Wrong number!')
+        else:
+            break
+    source_language = languages[source_language - 1]
 
-    destination_language = int(input("Type the number of language you want to translate to"
-                                     " or '0' to translate to all languages: "))
-    if destination_language not in range(14):
-        print('Wrong number!')
-        return 0
+    while True:
+        destination_language = int(input("Type the number of language you want to translate to"
+                                         " or '0' to translate to all languages: "))
+        if destination_language not in range(14):
+            print('Wrong number!')
+        else:
+            break
     if destination_language != 0:
-        destination_language = languages[destination_language-1]
+        destination_language = languages[destination_language - 1]
 
     word_to_translate = input('Type the word you want to translate: ').lower()
 
@@ -41,15 +44,17 @@ def manual():
     file.close()
 
 
-def auto():
-    source_language = args[1].capitalize()
-    destination_language = args[2].capitalize()
+def arguments():
+    source_language = argv[1].capitalize()
+    destination_language = argv[2].capitalize()
     if destination_language != 'All':
         if source_language not in languages:
             print(f"Sorry, the program doesn't support {source_language}")
+            exit()
         if destination_language not in languages:
             print(f"Sorry, the program doesn't support {destination_language}")
-    word_to_translate = args[3].lower()
+            exit()
+    word_to_translate = argv[3].lower()
     file = open(f'{word_to_translate}.txt', 'w', encoding='utf-8')
 
     if destination_language == 'All':
@@ -63,23 +68,22 @@ def auto():
 
 
 def translate(source_language, destination_language, word_to_translate, file):
-    request = requests.get(f'https://context.reverso.net/translation/{source_language.lower()}-'
-                           f'{destination_language.lower()}/{word_to_translate}', headers={'User-Agent': 'Mozilla/5.0'})
-
+    request = get(f'https://context.reverso.net/translation/{source_language.lower()}-'
+                  f'{destination_language.lower()}/{word_to_translate}', headers={'User-Agent': 'Mozilla/5.0'})
     if request:
         html_code = request.content
         soup = BeautifulSoup(html_code, 'html.parser')
 
         div_words = soup.find('div', id='translations-content')
         if div_words is None:
-            print(f'Sorry, unable to find {word_to_translate}')
-            return 0
+            print(f'Sorry, unable to find {word_to_translate} or this language pair is not supported')
+            exit()
         words = div_words.find_all('a', limit=5)
         for i, word in enumerate(words):
             words[i] = word.get_text().strip()
 
         source_examples = soup.find_all('div', {'class': 'src ltr'}, limit=5)
-        regex = re.compile(r"^trg [rtl]{3}")
+        regex = compile(r"^trg [rtl]{3}")
         destination_examples = soup.find_all('div', {'class': regex}, limit=5)
         for i, example in enumerate(source_examples):
             source_examples[i] = example.get_text().strip()
@@ -105,8 +109,7 @@ def translate(source_language, destination_language, word_to_translate, file):
         print('Something wrong with your internet connection')
 
 
-args = sys.argv
-if len(args) == 4:
-    auto()
+if len(argv) == 4:
+    arguments()
 else:
-    manual()
+    console()
